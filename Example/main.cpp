b387051880b5f1c4f39a1237f93f267b61b69672
Lefteris Eleftheriades <lefteris.eleftheriades@gmail.com>
2024-08-16 00:05:37 +0300

    Updated font of main form

--- Example/main.cpp
+++ Example/main.cpp
@@ -9,9 +9,14 @@ int main(int argc, char* argv[]) {
 
     WindowClass mainWindowClass(L"mainWindow");
     Window window(mainWindowClass, L"main window", 185, 170, WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU );
+    Font segoe(L"Segoe UI");
     Button btnStandardControls(window, L"Standard Controls",10, 10, 150,30);
     Button btnCommonControls(  window, L"Common Controls",  10, 50, 150, 30);
     Button btnDraw(            window, L"Draw",             10, 90, 150, 30);    
+
+    btnStandardControls.setFont(segoe);
+    btnCommonControls.setFont(segoe);
+    btnDraw.setFont(segoe);
 
     btnStandardControls.setCommand(window,
         [&](int e) {
